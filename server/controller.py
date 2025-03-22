07bbd7824e6135fb5ab65d2f15d36ff728d21291
tryiou <tryou@tryou.com>
2025-03-22 16:24:22 +0100

    change aiorpcx_version check to allow newer versions.

--- server/controller.py
+++ server/controller.py
@@ -19,8 +19,8 @@ class Controller(ServerBase):
         '''Start the RPC server and wait for the mempool to synchronize.  Then
         start serving external clients.
         '''
-        if not (0, 22, 0) <= aiorpcx_version < (0, 23):
-            raise RuntimeError('aiorpcX version 0.22.x is required')
+        if not (0, 22, 0) <= aiorpcx_version:
+            raise RuntimeError('aiorpcX version >0.22.0 is required')
 
         env = self.env
         min_str, max_str = env.coin.SESSIONCLS.protocol_min_max_strings()
